4a1d63e12102d3a83af5e946dc858781cc6bc0f0
Md Anam Raihan <md.anam.raihan@ibm.com>
2026-07-29 20:07:59 +0530

    testing skiping basic example test

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -25,7 +25,7 @@ func setupOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptio
 }
 
 func TestRunBasicExample(t *testing.T) {
-	t.Parallel()
+	t.Skip()
 
 	options := setupOptions(t, "isv-basic", basicExampleDir)
 
